13b2a172e7ab8a0cb048c15c52741e00cc4ed5f6
valmsmith39a <valmsmith39a@gmail.com>
2020-04-16 22:45:29 -0700

    company news sentiment route

--- server.py
+++ server.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 def hello_world():
 	return "Monkeys throwing darts"
 
+@app.route('/company-news-sentiment')
+def company_news_sentiment():
+	return "Tesla (TSLA) news sentiment is negative (2 positive, 8 negative)"
+
